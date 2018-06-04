2c262680f00db3641ba176cb435a54ef2b153101
Nutz95 <hexman@free.fr>
2018-06-04 20:14:14 +0200

    -Adding materials profiles for 2.85mm filament -adding custom parameters to expose hidden setting from features.lua file

--- fff/scalar_l/header.gcode
+++ fff/scalar_l/header.gcode
@@ -2,8 +2,9 @@ G21  ; set units to millimeters
 G90  ; use absolute coordinates
 M82  ; use absolute distances for extrusion
 M107 ; fan off
-M190 S<HBPTEMP> ; wait for bed temperature to be reached
 M104 S<TOOLTEMP> ; set temperature
+M190 S<HBPTEMP> ; wait for bed temperature to be reached
+
 G28  ; home all axes
 G29  ; AutoBedLeveling
 G92 E0
---
+++ fff/scalar_l/materials/abs-08-285.lua
@@ -0,0 +1,16 @@
+name_en = "ABS [2.85-0.8mm]"
+name_es = "ABS [2.85-0.8mm]"
+name_fr = "ABS [2.85-0.8mm]"
+name_ch = "ABS [2.85-0.8mm]"
+
+extruder_temp_degree_c_0 = 250
+filament_diameter_mm_0 = 2.85
+filament_priming_mm_0 = 1.5
+
+bed_temp_degree_c = 110
+add_raft=true
+
+enable_fan = false
+enable_fan_first_layer = false
+fan_speed_percent = 0
+fan_speed_percent_on_bridges = 0
