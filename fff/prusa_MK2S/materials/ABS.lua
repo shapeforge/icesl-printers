91a8347bd5b13b1aa656db996cc4fe96408cb33b
Pierre Bedell <pierre.bedell@gmail.com>
2018-10-29 10:45:13 +0100

    cooling fan speed per material

--- fff/prusa_MK2S/materials/ABS.lua
+++ fff/prusa_MK2S/materials/ABS.lua
@@ -6,3 +6,8 @@ extruder_temp_degree_c_0 = 230
 bed_temp_degree_c = 100
 
 filament_priming_mm_0 = 0.6
+
+-- enable fan only if the printer is enclosed !
+enable_fan = false
+fan_speed_percent = 50
+fan_speed_percent_on_bridges = 50
