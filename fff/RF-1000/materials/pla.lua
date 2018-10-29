6fd4dceeb6b9c5a49be1180fee4e7fa07eb24caf
Jimmy ETIENNE <jimmy.etienne@inria.fr>
2018-10-29 10:36:23 +0100

    RF-1000 by Guido Essers

---
+++ fff/RF-1000/materials/pla.lua
@@ -0,0 +1,10 @@
+name_en = "PLA"
+name_es = "PLA"
+name_fr = "PLA"
+name_ch = "PLA"
+
+extruder_temp_degree_c_0 = 210
+filament_diameter_mm_0 = 2.85
+filament_priming_mm_0 = 3.0
+
+bed_temp_degree_c = 50
