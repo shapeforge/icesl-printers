cd082317a6f5200b65dfb634025af0c063ddd153
Pierre Bedell <pierre.bedell@gmail.com>
2019-02-18 15:41:08 +0100

    Ultimaker2 profile tweaks: Header updated to support nozzle diameter and M207/208 Firmware retraction values (specified using priming_mm_per_sec and filament_priming_mm_0) | General profile cleanup (convertion to mm3 exported in it's own function for readability)

--- fff/Ultimaker2/printer.lua
+++ fff/Ultimaker2/printer.lua
@@ -3,30 +3,42 @@
 
 version = 2
 
-extruder_e = 0
-extruder_e_restart = 0
-reset_e_on_next_prime = false
-
 function comment(text)
   output('; ' .. text)
 end
 
 to_cubic_mm = 1
 
+function to_mm_cube(e)
+  local r = filament_diameter_mm[extruders[0]] / 2
+  return (math.pi * r^2 ) * e
+end
+
+function round(number, decimals)
+  local power = 10^decimals
+  return math.floor(number * power) / power
+end
+
 function header()
-  r = filament_diameter_mm[extruders[0]] / 2
-  to_mm_cube = 3.14159 * r * r
   output(';FLAVOR:UltiGCode')
   output(';TIME:' .. time_sec)
-  output(';MATERIAL:' .. to_mm_cube*filament_tot_length_mm[extruders[0]])
+  output(';MATERIAL:' .. to_mm_cube(filament_tot_length_mm[extruders[0]]) )
   output(';MATERIAL2:0')
+  output(';NOZZLE_DIAMETER:' .. round(nozzle_diameter_mm,2))
   output('M107')
+  output('M207 F' .. priming_mm_per_sec * 60 .. ' S' .. filament_priming_mm_0)
+  output('M208 F' .. priming_mm_per_sec * 60 .. ' S' .. filament_priming_mm_0)
+  output('M82')
 end
 
 function footer()
-  output('M107')
   output('G10')
+  output('M107')
 end
+
+reset_e_on_next_prime = false
+extruder_e = 0
+extruder_e_reset = 0
 
 function retract(extruder,e)
   extruder_e = e
@@ -37,9 +49,9 @@ end
 function prime(extruder,e)
   extruder_e = e
   output('G11')
-  if reset_e_on_next_prime then
+  if reset_e_on_next_prime == true then
     output('G92 E0')
-    extruder_e_restart = extruder_e
+    extruder_e_reset = extruder_e
     reset_e_on_next_prime = false
   end
   return e
@@ -50,7 +62,7 @@ current_extruder = 0
 current_frate = 600
 
 function layer_start(zheight)
-  output(';(<layer>)')
+  output(';<layer ' .. layer_id .. '>')
   if layer_id == 0 then
     output('G0 F600 Z' .. ff(zheight))
   else
@@ -61,7 +73,7 @@ end
 
 function layer_stop()
   reset_e_on_next_prime = true
-  output(';(</layer>)')
+  output(';</layer ' .. layer_id .. '>')
 end
 
 function select_extruder(extruder)
@@ -80,24 +92,20 @@ function move_xyz(x,y,z)
 end
 
 function move_xyze(x,y,z,e)
-  extruder_e = e
-  r = filament_diameter_mm[extruders[0]] / 2
-  to_mm_cube = 3.14159 * r * r
   letter = 'E'
+  extruder_e = e
   if z == current_z then
-    output('G1 F' .. f(current_frate) .. ' X' .. f(x) .. ' Y' .. f(y) .. ' ' .. letter .. ff((e - extruder_e_restart)*to_mm_cube))
+    output('G1 F' .. f(current_frate) .. ' X' .. f(x) .. ' Y' .. f(y) .. ' ' .. letter .. ff(to_mm_cube(e - extruder_e_reset)) )
   else
-    output('G1 F' .. f(current_frate) .. ' X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. ff(z) .. ' ' .. letter .. ff((e - extruder_e_restart)*to_mm_cube))
+    output('G1 F' .. f(current_frate) .. ' X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. ff(z) .. ' ' .. letter .. ff(to_mm_cube(e - extruder_e_reset)) )
     current_z = z
   end
 end
 
 function move_e(e)
-  extruder_e = e
   letter = 'E'
-  r = filament_diameter_mm[extruders[0]] / 2
-  to_mm_cube = 3.14159 * r * r
-  output('G1 ' .. letter .. ff((e - extruder_e_restart)*to_mm_cube))
+  extruder_e = e
+  output('G1 ' .. letter .. ff(to_mm_cube(e - extruder_e_reset)))
 end
 
 function set_feedrate(feedrate)
