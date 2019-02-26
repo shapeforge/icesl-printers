73e420cfaac49a1e7e4ca21e5fb752c35f668938
Pierre Bedell <pierre.bedell@gmail.com>
2019-02-26 16:01:10 +0100

    profile fixed to use the proper computation for brushes - removed filament diameter compensation

--- fff/prusa_diamond5_brushes/features.lua
+++ fff/prusa_diamond5_brushes/features.lua
@@ -8,23 +8,30 @@ bed_size_z_mm = 160
 -- Printer Extruder
 filament_diameter = 1.75
 nozzle_diameter_mm = 0.4
-extruder_count = 5 -- number of inputed filament in the nozzle
-
-gen_tower = false
-enable_active_temperature_control = false
-gen_shield = true
+extruder_count = 5 -- number of separated virtual extruders (to affect a color to a brush)
+nb_filament_in = 5 -- number of inputed filament in the nozzle
 
 z_offset   = 0.0
 
+enable_active_temperature_control = false
+gen_shield = false
+
+-- Purge tower settings
+gen_tower = true
+tower_side_x_mm = 25.0
+tower_side_y_mm = 20.0
+tower_brim_num_contours = 5
+
 -- Default Retraction Settings
 filament_priming_mm = 6
-priming_mm_per_sec = 50
+priming_mm_per_sec = 100
 
--- Layer height limits
+-- Layer height settings
+z_layer_height_mm = 0.3
 z_layer_height_mm_min = nozzle_diameter_mm * 0.125
 z_layer_height_mm_max = nozzle_diameter_mm * 0.75
 
--- Printing speed limits
+-- Printing speed settings
 print_speed_mm_per_sec_min = 5
 print_speed_mm_per_sec_max = 80
 
@@ -35,14 +42,19 @@ first_layer_print_speed_mm_per_sec = 10
 first_layer_print_speed_mm_per_sec_min = 1
 first_layer_print_speed_mm_per_sec_max = 80
 
--- Printing temperatures limits
-extruder_temp_degree_c = 210
+-- Printing temperatures settings
+extruder_temp_degree_c = 200
 extruder_temp_degree_c_min = 150
-extruder_temp_degree_c_max = 270
+extruder_temp_degree_c_max = 250
 
 bed_temp_degree_c = 50
 bed_temp_degree_c_min = 0
 bed_temp_degree_c_max = 120
+
+-- Cooling settings
+enable_fan = true
+fan_speed_percent = 100
+fan_speed_percent_on_bridges = 100
 
 for i=0,63,1 do
   _G['filament_diameter_mm_'..i] = filament_diameter
@@ -50,28 +62,30 @@ for i=0,63,1 do
   _G['extruder_temp_degree_c_' ..i] = extruder_temp_degree_c
   _G['extruder_temp_degree_c_'..i..'_min'] = extruder_temp_degree_c_min
   _G['extruder_temp_degree_c_'..i..'_max'] = extruder_temp_degree_c_max
-  _G['extruder_mix_count_'..i] = extruder_count
+  _G['extruder_mix_count_'..i] = nb_filament_in
 end
 
 
 add_checkbox_setting('auto_bed_leveling', 'Auto Bed Levelling sensor','Auto Levelling Sensor (BLTouch, Pinda, capacitive sensor, etc.)')
 
---add_checkbox_setting('filament_diameter_management', 'Manage Filament Diameters','Manage the diameter of your filaments (if you use custum made filaments)')
-filament_diameter_management = false
-
-if filament_diameter_management == true then
-	add_setting('filament_diameter_A','filament diameter extruder A',1.0,2.0)
-	filament_diameter_A = 1.75
-	
-	add_setting('filament_diameter_B','filament diameter extruder B',1.0,2.0)
-	filament_diameter_B = 1.75
-	
-	add_setting('filament_diameter_C','filament diameter extruder C',1.0,2.0)
-	filament_diameter_C = 1.75
-	
-	add_setting('filament_diameter_D','filament diameter extruder D',1.0,2.0)
-	filament_diameter_D = 1.75
-	
-	add_setting('filament_diameter_H','filament diameter extruder H',1.0,2.0)
-	filament_diameter_H = 1.75
-end
+-- default parameters
+print_speed_mm_per_sec = 10
+perimeter_print_speed_mm_per_sec = 10
+first_layer_print_speed_mm_per_sec = 10
+print_speed_microlayers_mm_per_sec = 40
+travel_speed_mm_per_sec = 80
+add_brim = true
+extruder_purge_volume_mm3 = 10
+shield_distance_to_part_mm = 2
+mixing_shield_speed_multiplier = 1
+filament_priming_mm_0 = 6
+travel_max_length_without_retract = 5
+extruder_swap_zlift_mm = 0
+extruder_swap_retract_length_mm = 0
+flow_dampener_path_length_end_mm = 6
+flow_dampener_path_length_start_mm = 6
+flow_dampener_e_length_mm = 6
+print_kickback_length_mm = 0
+mixing_wipe_length_mm = 0
+enable_fit_single_path = true
+path_width_speed_adjustement_exponent = 1
