ff66ecd31cfb1d17520f8c28df41f7e67e2490be
dbatab <dbatab@users.noreply.github.com>
2019-05-06 09:57:11 +0200

    Deleted folder

--- raise3d_pro2/profiles/SingleWall_Perimeters.lua
+++
@@ -1,9 +0,0 @@
-name_en = "Perimeter only - single wall"
-name_fr = "Perimeter only - single wall"
-name_es = "Perimeter only - single wall"
-
--- perimeters & infill
-num_shells_0 = 0
-cover_thickness_mm_0 = 0
-infill_percentage_0 = 0
-print_perimeter_0 = true
