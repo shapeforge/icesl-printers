d4079fb8caec649b7a186a105390b2ff7b63351f
dbatab <44642588+dbatab@users.noreply.github.com>
2019-05-06 09:47:07 +0200

    Add files via upload

---
+++ fff/raise3d_pro2/materials/Formfutura_EasyCork.lua
@@ -0,0 +1,46 @@
+name_en = "EasyCork"
+name_fr = "EasyCork"
+name_es = "EasyCork"
+
+-- filament/nozzle dimensions and extruder side
+filament_diameter_mm_0 = 1.8
+filament_diameter_mm_1 = 1.8
+nozzle_diameter_mm = 0.4
+extruder_0 = 1
+infill_extruder_0 = 1
+
+-- temps and layer heigth
+retract_after_z = 0
+z_offset = 0.0
+z_layer_height_mm = 0.25
+extruder_temp_degree_c_0 = 225
+extruder_temp_degree_c_1 = 225
+bed_temp_degree_c = 60
+
+-- speeds and multipliers
+print_speed_mm_per_sec = 35
+perimeter_print_speed_mm_per_sec = 35
+first_layer_print_speed_mm_per_sec = 25
+travel_speed_mm_per_sec = 80
+flow_multiplier_0 = 1.05
+speed_multiplier_0 = 1.0
+
+-- retraction settings
+filament_priming_mm_0 = 1.5
+priming_mm_per_sec = 60
+
+-- fan settings
+enable_fan = true
+enable_fan_first_layer = false
+fan_speed_percent = 75
+fan_speed_percent_on_bridges = 100
+
+-- secondary settings
+add_raft = false
+raft_spacing = 1.0
+gen_supports = false
+support_extruder = 0
+add_brim = true
+brim_distance_to_print = 2.0
+brim_num_contours = 2
+process_thin_features = false
