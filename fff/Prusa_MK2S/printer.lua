6c75ba7f78d27b02e31a45457d34d98a2a6345c3
Pierre Bedell <pierre.bedell@gmail.com>
2019-07-17 15:05:30 +0200

    continuous z printing fix

--- fff/Prusa_MK2S/printer.lua
+++ fff/Prusa_MK2S/printer.lua
@@ -3,13 +3,26 @@
 
 extruder_e = 0
 extruder_e_restart = 0
+current_z = 0.0
+
+changed_frate = false
+processing = false
+
+current_extruder = 0
+current_frate = 0
+
+current_fan_speed = -1
+
+craftware_debug = true
+
+--##################################################
 
 function comment(text)
   output('; ' .. text)
 end
 
 function header()
-  h = file('header.gcode')
+  local h = file('header.gcode')
   h = h:gsub('<TOOLTEMP>', extruder_temp_degree_c[extruders[0]])
   h = h:gsub('<HBPTEMP>', bed_temp_degree_c)
   output(h)
@@ -21,8 +34,8 @@ end
 
 function retract(extruder,e)
   output(';retract')
-  len   = filament_priming_mm[extruder]
-  speed = priming_mm_per_sec * 60
+  local len   = filament_priming_mm[extruder]
+  local speed = priming_mm_per_sec * 60
   output('G0 F' .. speed .. ' E' .. ff(e - len - extruder_e_restart))
   extruder_e = e - len
   return e - len
@@ -30,8 +43,8 @@ end
 
 function prime(extruder,e)
   output(';prime')
-  len   = filament_priming_mm[extruder]
-  speed = priming_mm_per_sec * 60
+  local len   = filament_priming_mm[extruder]
+  local speed = priming_mm_per_sec * 60
   output('G0 F' .. speed .. ' E' .. ff(e + len - extruder_e_restart))
   extruder_e = e + len
   return e + len
@@ -52,9 +65,6 @@ function layer_stop()
   output(';(</layer>)')
 end
 
-current_extruder = 0
-current_frate = 0
-
 function select_extruder(extruder)
 end
 
@@ -62,24 +72,98 @@ function swap_extruder(from,to,x,y,z)
 end
 
 function move_xyz(x,y,z)
-  output(';travel')
-  output('G0 F' .. f(current_frate) .. ' X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. ff(z))
+    --output(';travel')
+  if processing == true then
+    processing = false
+    comment('travel')
+  end
+
+  if z == current_z then
+    if changed_frate == true then 
+      output('G0 F' .. current_frate .. ' X' .. f(x) .. ' Y' .. f(y))
+      changed_frate = false
+    else
+      output('G0 X' .. f(x) .. ' Y' .. f(y))
+    end
+  else
+    if changed_frate == true then
+      output('G0 F' .. current_frate .. ' X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. ff(z))
+      changed_frate = false
+    else
+      output('G0 X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. ff(z))
+    end
+    current_z = z
+  end
 end
 
 function move_xyze(x,y,z,e)
   extruder_e = e
-  letter = 'E'
-  output('G1 F' .. current_frate .. ' X' .. f(x) .. ' Y' .. f(y) .. ' ' .. letter .. ff((e-extruder_e_restart)))
+
+  local e_value = extruder_e - extruder_e_restart
+
+  if processing == false then 
+    processing = true
+    if craftware_debug == true then
+      if      path_is_perimeter then output(';segType:Perimeter')
+      elseif  path_is_shell     then output(';segType:HShell')
+      elseif  path_is_infill    then output(';segType:Infill')
+      elseif  path_is_raft      then output(';segType:Raft')
+      elseif  path_is_brim      then output(';segType:Skirt')
+      elseif  path_is_shield    then output(';segType:Pillar')
+      elseif  path_is_support   then output(';segType:Support')
+      elseif  path_is_tower     then output(';segType:Pillar')
+      end
+    else
+      if      path_is_perimeter then comment('perimeter')
+      elseif  path_is_shell     then comment('shell')
+      elseif  path_is_infill    then comment('infill')
+      elseif  path_is_raft      then comment('raft')
+      elseif  path_is_brim      then comment('brim')
+      elseif  path_is_shield    then comment('shield')
+      elseif  path_is_support   then comment('support')
+      elseif  path_is_tower     then comment('tower')
+      end
+    end
+  end
+
+  if z == current_z then
+    if changed_frate == true then 
+      output('G1 F' .. current_frate .. ' X' .. f(x) .. ' Y' .. f(y) .. ' E' .. ff(e_value))
+      changed_frate = false
+    else
+      output('G1 X' .. f(x) .. ' Y' .. f(y) .. ' E' .. ff(e_value))
+    end
+  else
+    if changed_frate == true then
+      output('G1 F' .. current_frate .. ' X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. ff(z) .. ' E' .. ff(e_value))
+      changed_frate = false
+    else
+      output('G1 X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. ff(z) .. ' E' .. ff(e_value))
+    end
+    current_z = z
+  end
 end
 
 function move_e(e)
   extruder_e = e
-  letter = 'E'
-  output('G0 ' .. letter .. ff(e-extruder_e_restart))
+
+  local e_value =  extruder_e - extruder_e_restart
+
+  if changed_frate == true then 
+    output('G1 F' .. current_frate .. ' E' .. ff(e_value))
+    changed_frate = false
+  else
+    output('G1 E' .. ff(e_value))
+  end
 end
 
 function set_feedrate(feedrate)
-  current_frate = feedrate
+  if feedrate ~= current_frate then
+    current_frate = feedrate
+    changed_frate = true
+  else
+    changed_frate = false
+  end
 end
 
 function extruder_start()
@@ -92,10 +176,9 @@ function progress(percent)
 end
 
 function set_extruder_temperature(extruder,temperature)
-  output('M104 S' .. temperature .. ' T' .. extruder)
+  output('M104 S' .. temperature)
 end
 
-current_fan_speed = -1
 function set_fan_speed(speed)
   if speed ~= current_fan_speed then
     output('M106 S'.. math.floor(255 * speed/100))
--- fff/Prusa_MK3S/printer.lua
+++ fff/Prusa_MK3S/printer.lua
@@ -3,13 +3,26 @@
 
 extruder_e = 0
 extruder_e_restart = 0
+current_z = 0.0
+
+changed_frate = false
+processing = false
+
+current_extruder = 0
+current_frate = 0
+
+current_fan_speed = -1
+
+craftware_debug = true
+
+--##################################################
 
 function comment(text)
   output('; ' .. text)
 end
 
 function header()
-  h = file('header.gcode')
+  local h = file('header.gcode')
   h = h:gsub('<TOOLTEMP>', extruder_temp_degree_c[extruders[0]])
   h = h:gsub('<HBPTEMP>', bed_temp_degree_c)
   if flow_compensation == true then
@@ -25,7 +38,7 @@ function header()
 end
 
 function footer()
-  f = file('footer.gcode')
+  local f = file('footer.gcode')
   if flow_compensation == true then
     f = f:gsub('<FLOW>', 'M221 S100')
   else
@@ -36,8 +49,8 @@ end
 
 function retract(extruder,e)
   output(';retract')
-  len   = filament_priming_mm[extruder]
-  speed = priming_mm_per_sec * 60
+  local len   = filament_priming_mm[extruder]
+  local speed = priming_mm_per_sec * 60
   output('G0 F' .. speed .. ' E' .. ff(e - len - extruder_e_restart))
   extruder_e = e - len
   return e - len
@@ -45,8 +58,8 @@ end
 
 function prime(extruder,e)
   output(';prime')
-  len   = filament_priming_mm[extruder]
-  speed = priming_mm_per_sec * 60
+  local len   = filament_priming_mm[extruder]
+  local speed = priming_mm_per_sec * 60
   output('G0 F' .. speed .. ' E' .. ff(e + len - extruder_e_restart))
   extruder_e = e + len
   return e + len
@@ -67,9 +80,6 @@ function layer_stop()
   output(';(</layer>)')
 end
 
-current_extruder = 0
-current_frate = 0
-
 function select_extruder(extruder)
 end
 
@@ -77,24 +87,98 @@ function swap_extruder(from,to,x,y,z)
 end
 
 function move_xyz(x,y,z)
-  output(';travel')
-  output('G0 F' .. f(current_frate) .. ' X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. ff(z))
+    --output(';travel')
+  if processing == true then
+    processing = false
+    comment('travel')
+  end
+
+  if z == current_z then
+    if changed_frate == true then 
+      output('G0 F' .. current_frate .. ' X' .. f(x) .. ' Y' .. f(y))
+      changed_frate = false
+    else
+      output('G0 X' .. f(x) .. ' Y' .. f(y))
+    end
+  else
+    if changed_frate == true then
+      output('G0 F' .. current_frate .. ' X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. ff(z))
+      changed_frate = false
+    else
+      output('G0 X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. ff(z))
+    end
+    current_z = z
+  end
 end
 
 function move_xyze(x,y,z,e)
   extruder_e = e
-  letter = 'E'
-  output('G1 F' .. current_frate .. ' X' .. f(x) .. ' Y' .. f(y) .. ' ' .. letter .. ff((e-extruder_e_restart)))
+
+  local e_value = extruder_e - extruder_e_restart
+
+  if processing == false then 
+    processing = true
+    if craftware_debug == true then
+      if      path_is_perimeter then output(';segType:Perimeter')
+      elseif  path_is_shell     then output(';segType:HShell')
+      elseif  path_is_infill    then output(';segType:Infill')
+      elseif  path_is_raft      then output(';segType:Raft')
+      elseif  path_is_brim      then output(';segType:Skirt')
+      elseif  path_is_shield    then output(';segType:Pillar')
+      elseif  path_is_support   then output(';segType:Support')
+      elseif  path_is_tower     then output(';segType:Pillar')
+      end
+    else
+      if      path_is_perimeter then comment('perimeter')
+      elseif  path_is_shell     then comment('shell')
+      elseif  path_is_infill    then comment('infill')
+      elseif  path_is_raft      then comment('raft')
+      elseif  path_is_brim      then comment('brim')
+      elseif  path_is_shield    then comment('shield')
+      elseif  path_is_support   then comment('support')
+      elseif  path_is_tower     then comment('tower')
+      end
+    end
+  end
+
+  if z == current_z then
+    if changed_frate == true then 
+      output('G1 F' .. current_frate .. ' X' .. f(x) .. ' Y' .. f(y) .. ' E' .. ff(e_value))
+      changed_frate = false
+    else
+      output('G1 X' .. f(x) .. ' Y' .. f(y) .. ' E' .. ff(e_value))
+    end
+  else
+    if changed_frate == true then
+      output('G1 F' .. current_frate .. ' X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. ff(z) .. ' E' .. ff(e_value))
+      changed_frate = false
+    else
+      output('G1 X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. ff(z) .. ' E' .. ff(e_value))
+    end
+    current_z = z
+  end
 end
 
 function move_e(e)
   extruder_e = e
-  letter = 'E'
-  output('G0 ' .. letter .. ff(e-extruder_e_restart))
+
+  local e_value =  extruder_e - extruder_e_restart
+
+  if changed_frate == true then 
+    output('G1 F' .. current_frate .. ' E' .. ff(e_value))
+    changed_frate = false
+  else
+    output('G1 E' .. ff(e_value))
+  end
 end
 
 function set_feedrate(feedrate)
-  current_frate = feedrate
+  if feedrate ~= current_frate then
+    current_frate = feedrate
+    changed_frate = true
+  else
+    changed_frate = false
+  end
 end
 
 function extruder_start()
@@ -107,10 +191,9 @@ function progress(percent)
 end
 
 function set_extruder_temperature(extruder,temperature)
-  output('M104 S' .. temperature .. ' T' .. extruder)
+  output('M104 S' .. temperature)
 end
 
-current_fan_speed = -1
 function set_fan_speed(speed)
   if speed ~= current_fan_speed then
     output('M106 S'.. math.floor(255 * speed/100))
