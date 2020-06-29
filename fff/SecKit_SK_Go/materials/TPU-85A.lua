d8fef31dc43fbe14f2cd37dd04e92dcff5b728de
pahugron <61386988+pahugron@users.noreply.github.com>
2020-06-29 17:24:29 +0200

    Seckit SK-Go profile
    * Seckit SK-Go Profile added
    * small fixes to the CR10 S5 profile

---
+++ fff/SecKit_SK_Go/materials/TPU-85A.lua
@@ -0,0 +1,21 @@
+name_en = "TPU-85A"
+name_fr = "TPU-85A"
+name_es = "TPU-85A"
+
+filament_linear_adv_factor = 0
+
+extruder_temp_degree_c_0 = 240
+bed_temp_degree_c = 40
+
+filament_priming_mm_0 = 0.8
+
+enable_fan = true
+fan_speed_percent = 100
+fan_speed_percent_on_bridges = 100
+
+print_speed_mm_per_sec = 35
+perimeter_print_speed_mm_per_sec = 35
+first_layer_print_speed_mm_per_sec = 25
+
+flow_multiplier_0 = 1.25
+speed_multiplier_0 = 1.25
---
+++ fff/SecKit_SK_Go/profiles/high.lua
@@ -0,0 +1,16 @@
+name_en = "High quality"
+name_fr = "Haute qualité"
+name_es = "Alta calidad"
+
+z_layer_height_mm = 0.15
+
+print_speed_mm_per_sec = 60
+perimeter_print_speed_mm_per_sec = 40
+first_layer_print_speed_mm_per_sec = 20
+
+travel_speed_mm_per_sec = 120
+
+num_shells_0 = 3
+cover_thickness_mm_0 = 2
+infill_percentage_0 = 20
+print_perimeter_0 = true
