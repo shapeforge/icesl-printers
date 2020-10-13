9c4e51501c98eafdc1c910cf078b8445bf3b82ff
Pierre Bedell <pierre.bedell@gmail.com>
2020-10-13 17:48:47 +0200

    update and paths tags

--- fff/CR10S_Pro/features.lua
+++ fff/CR10S_Pro/features.lua
@@ -1,36 +1,54 @@
+-- Creality CR10S pro
+-- 30/07/2020
+
+-- Build Area dimensions
 bed_size_x_mm = 300
 bed_size_y_mm = 300
 bed_size_z_mm = 400
-nozzle_diameter_mm = 0.4
 
+-- Printer Extruder
 extruder_count = 1
+nozzle_diameter_mm = 0.4
+filament_diameter_mm = 1.75
 
-z_offset   = 0.0
-
+-- Retraction Settings
+filament_priming_mm = 0.4 -- min 0.5 - max 2
 priming_mm_per_sec = 45
 
-z_layer_height_mm_min = 0.05
-z_layer_height_mm_max = nozzle_diameter_mm * 0.75
+-- Layer height
+z_layer_height_mm = 0.2
+z_layer_height_mm_min = nozzle_diameter_mm * 0.10
+z_layer_height_mm_max = nozzle_diameter_mm * 0.90
 
-print_speed_mm_per_sec_min = 5
-print_speed_mm_per_sec_max = 80
+-- Printing temperatures
+extruder_temp_degree_c = 210
+extruder_temp_degree_c_min = 150
+extruder_temp_degree_c_max = 270
 
 bed_temp_degree_c = 50
 bed_temp_degree_c_min = 0
 bed_temp_degree_c_max = 120
 
+-- Printing speeds
+print_speed_mm_per_sec = 60
+print_speed_mm_per_sec_min = 5
+print_speed_mm_per_sec_max = 120
+
+perimeter_print_speed_mm_per_sec = 45
 perimeter_print_speed_mm_per_sec_min = 5
 perimeter_print_speed_mm_per_sec_max = 80
 
 first_layer_print_speed_mm_per_sec = 10
-first_layer_print_speed_mm_per_sec_min = 1
-first_layer_print_speed_mm_per_sec_max = 80
+first_layer_print_speed_mm_per_sec_min = 5
+first_layer_print_speed_mm_per_sec_max = 30
+
+travel_speed_mm_per_sec = 180
 
 for i = 0, max_number_extruders, 1 do
-  _G['filament_diameter_mm_'..i] = 1.75
-  _G['filament_priming_mm_'..i] = 4.0 -- 0.8 for direct drive extruder
-  _G['extruder_temp_degree_c_' ..i] = 210
-  _G['extruder_temp_degree_c_'..i..'_min'] = 150
-  _G['extruder_temp_degree_c_'..i..'_max'] = 270
+  _G['filament_diameter_mm_'..i] = filament_diameter_mm
+  _G['filament_priming_mm_'..i] = filament_priming_mm
+  _G['extruder_temp_degree_c_' ..i] = extruder_temp_degree_c
+  _G['extruder_temp_degree_c_'..i..'_min'] = extruder_temp_degree_c_min
+  _G['extruder_temp_degree_c_'..i..'_max'] = extruder_temp_degree_c_max
   _G['extruder_mix_count_'..i] = 1
 end
--- fff/CR10S_Pro/printer.lua
+++ fff/CR10S_Pro/printer.lua
@@ -1,17 +1,44 @@
--- Generic reprap
+-- Creality CR10S pro
+-- 30/07/2020
+
+extruder_e = 0
+extruder_e_restart = 0
+current_z = 0.0
+
+processing = false
+
+current_extruder = 0
+current_frate = 0
+
+current_fan_speed = -1
+
+path_type = {
+--{ 'default',    'Craftware'}
+  { ';perimeter',  ';segType:Perimeter' },
+  { ';shell',      ';segType:HShell' },
+  { ';infill',     ';segType:Infill' },
+  { ';raft',       ';segType:Raft' },
+  { ';brim',       ';segType:Skirt' },
+  { ';shield',     ';segType:Pillar' },
+  { ';support',    ';segType:Support' },
+  { ';tower',      ';segType:Pillar'}
+}
+
+craftware_debug = true -- allow the use of Craftware paths naming convention
+
+--##################################################
 
 function comment(text)
   output('; ' .. text)
 end
-
-extruder_e = 0
-extruder_e_restart = 0
 
 function header()
   h = file('header.gcode')
   h = h:gsub( '<TOOLTEMP>', extruder_temp_degree_c[extruders[0]] )
   h = h:gsub( '<HBPTEMP>', bed_temp_degree_c )
   output(h)
+  current_frate = travel_speed_mm_per_sec * 60
+  changed_frate = true
 end
 
 function footer()
@@ -19,36 +46,38 @@ function footer()
 end
 
 function layer_start(zheight)
-  comment('<layer>')
-  output('G1 Z' .. f(zheight))
+  local frate = 600
+  comment('<layer ' .. layer_id ..'>')
+  output('G1 F' .. frate .. ' Z' .. f(zheight))
+  current_frate = frate
+  changed_frate = true
 end
 
 function layer_stop()
   extruder_e_restart = extruder_e
   output('G92 E0')
-  comment('</layer>')
+  comment('<layer ' .. layer_id ..'>')
 end
 
 function retract(extruder,e)
-  len   = filament_priming_mm[extruder]
-  speed = priming_mm_per_sec[extruder] * 60;
-  letter = 'E'
-  output('G1 F' .. speed .. ' ' .. letter .. ff(e - len - extruder_e_restart))
+  output(';retract')
+  local len   = filament_priming_mm[extruder]
+  local speed = priming_mm_per_sec[extruder] * 60
+  output('G1 F' .. speed .. ' E' .. ff(e - len - extruder_e_restart))
   extruder_e = e - len
+  current_frate = speed
   return e - len
 end
 
 function prime(extruder,e)
-  len   = filament_priming_mm[extruder]
-  speed = priming_mm_per_sec[extruder] * 60;
-  letter = 'E'
-  output('G1 F' .. speed .. ' ' .. letter .. ff(e + len - extruder_e_restart))
+  output(';prime')
+  local len   = filament_priming_mm[extruder]
+  local speed = priming_mm_per_sec[extruder] * 60
+  output('G1 F' .. speed .. ' E' .. ff(e + len - extruder_e_restart))
   extruder_e = e + len
+  current_frate = speed
   return e + len
 end
-
-current_extruder = 0
-current_frate = 0
 
 function select_extruder(extruder)
 end
@@ -57,23 +86,44 @@ function swap_extruder(from,to,x,y,z)
 end
 
 function move_xyz(x,y,z)
-  output('G1 X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. f(z+z_offset))
+  if processing == true then
+    processing = false
+  end
+  output(';travel')
+  output('G0 F' .. current_frate .. ' X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. ff(z))
+  current_z = z
 end
 
 function move_xyze(x,y,z,e)
   extruder_e = e
-  letter = 'E'
-  output('G1 X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. f(z+z_offset) .. ' F' .. current_frate .. ' ' .. letter .. ff(e - extruder_e_restart))
+  local e_value = extruder_e - extruder_e_restart
+
+  if processing == false then 
+    processing = true
+    local p_type = 1 -- default paths naming
+    if craftware_debug then p_type = 2 end
+    if      path_is_perimeter then output(path_type[1][p_type])
+    elseif  path_is_shell     then output(path_type[2][p_type])
+    elseif  path_is_infill    then output(path_type[3][p_type])
+    elseif  path_is_raft      then output(path_type[4][p_type])
+    elseif  path_is_brim      then output(path_type[5][p_type])
+    elseif  path_is_shield    then output(path_type[6][p_type])
+    elseif  path_is_support   then output(path_type[7][p_type])
+    elseif  path_is_tower     then output(path_type[8][p_type])
+    end
+  end
+
+  output('G1 F' .. current_frate .. ' X' .. f(x) .. ' Y' .. f(y) .. ' Z' .. ff(z) .. ' E' .. ff(e_value))
+  current_z = z
 end
 
 function move_e(e)
   extruder_e = e
-  letter = 'E'
-  output('G1 ' .. letter .. ff(e - extruder_e_restart))
+  local e_value =  extruder_e - extruder_e_restart
+  output('G1 F' .. current_frate .. ' E' .. ff(e_value))
 end
 
 function set_feedrate(feedrate)
-  output('G1 F' .. feedrate)
   current_frate = feedrate
 end
 
@@ -87,14 +137,13 @@ function progress(percent)
 end
 
 function set_extruder_temperature(extruder,temperature)
-  output('M104 S' .. temperature .. ' T' .. extruder)
+  output('M104 S' .. temperature)
 end
 
 function set_and_wait_extruder_temperature(extruder,temperature)
-  output('M109 S' .. temperature .. ' T' .. extruder)
+  output('M109 S' .. temperature)
 end
 
-current_fan_speed = -1
 function set_fan_speed(speed)
   if speed ~= current_fan_speed then
     output('M106 S'.. math.floor(255 * speed/100))
