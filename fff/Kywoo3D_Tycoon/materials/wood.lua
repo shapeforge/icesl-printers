f031fa177de59676cb409937990ef82010a4fec8
Romeo Disca <romeo.disca@gmail.com>
2021-11-20 22:04:05 +0100

    changed wood extruder and bed temperatures

--- fff/Kywoo3D_Tycoon/materials/wood.lua
+++ fff/Kywoo3D_Tycoon/materials/wood.lua
@@ -2,8 +2,8 @@ name_en = "WOOD"
 name_es = "WOOD"
 name_fr = "WOOD"
 
-extruder_temp_degree_c_0 = 230
+extruder_temp_degree_c_0 = 210
 filament_diameter_mm_0 = 1.75
 filament_priming_mm_0 = 3.0
 
-bed_temp_degree_c = 100
+bed_temp_degree_c = 65
