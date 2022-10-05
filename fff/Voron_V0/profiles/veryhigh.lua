7f3e3ee28e0bc54acb2773afe7932bcbc5f6081d
Pierre Bedell <pierre.bedell@gmail.com>
2022-10-05 10:40:58 +0200

    profiles update & purge and header speeds fix

--- fff/Voron_V0/profiles/veryhigh.lua
+++ fff/Voron_V0/profiles/veryhigh.lua
@@ -5,13 +5,15 @@ name_fr = "Trés Haute qualité"
 z_layer_height_mm = 0.06
 
 print_speed_mm_per_sec = 60
-first_layer_print_speed_mm_per_sec = 20
-perimeter_print_speed_mm_per_sec= 30
-cover_print_speed_mm_per_sec= 30
-travel_speed_mm_per_sec= 160
+first_layer_print_speed_mm_per_sec = 30
+perimeter_print_speed_mm_per_sec = 20
+cover_print_speed_mm_per_sec = 30
+travel_speed_mm_per_sec = 160
 
-print_perimeter_0 = true
-num_shells_0 = 3
-cover_thickness_mm_0 = 2
-infill_type_0 = "Gyroid"
-infill_percentage_0 = 30
+-- affecting settings to all brushes
+for i = 0, max_number_brushes, 1 do
+  _G['print_perimeter_'..i] = true
+  _G['num_shells_' ..i] = 3
+  _G['cover_thickness_mm_'..i] = 1.8
+  _G['infill_percentage_'..i] = 30
+end
