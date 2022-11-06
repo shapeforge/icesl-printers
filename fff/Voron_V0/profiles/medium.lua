c6dacd1772450fe84207ad768dd0a7763181eba8
Pierre Bedell <pierre.bedell@gmail.com>
2022-11-06 22:13:15 +0100

    speeds and "medium"  quality profile tweaks

--- fff/Voron_V0/profiles/medium.lua
+++ fff/Voron_V0/profiles/medium.lua
@@ -2,18 +2,21 @@ name_en = "Standard quality"
 name_es = "Calidad estándar"
 name_fr = "Qualité standard"
 
+-- These settings are based on Voron's recommended printing settings
+-- https://docs.vorondesign.com/sourcing.html#print-settings
+
 z_layer_height_mm = 0.2
 
-print_speed_mm_per_sec = 80
-first_layer_print_speed_mm_per_sec = 20
-perimeter_print_speed_mm_per_sec = 40
-cover_print_speed_mm_per_sec = 40
+print_speed_mm_per_sec = 120
+first_layer_print_speed_mm_per_sec = 40
+perimeter_print_speed_mm_per_sec = 60
+cover_print_speed_mm_per_sec = 60
 travel_speed_mm_per_sec = 200
 
 -- affecting settings to all brushes
 for i = 0, max_number_brushes, 1 do
   _G['print_perimeter_'..i] = true
-  _G['num_shells_' ..i] = 2
-  _G['cover_thickness_mm_'..i] = 1.2
-  _G['infill_percentage_'..i] = 20
+  _G['num_shells_' ..i] = 3
+  _G['cover_thickness_mm_'..i] = 2.0
+  _G['infill_percentage_'..i] = 35
 end
