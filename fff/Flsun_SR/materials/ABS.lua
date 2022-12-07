8df6cd7ad16c96a44956bb366e0c3417c53b1019
Pierre Bedell <pierre.bedell@gmail.com>
2022-12-07 11:34:39 +0100

    spiral compatibility & quality+material profiles

--- fff/Flsun_SR/materials/ABS.lua
+++ fff/Flsun_SR/materials/ABS.lua
@@ -1,12 +1,12 @@
-name_en = "PLA"
-name_fr = "PLA"
-name_es = "PLA"
+name_en = "ABS-ASA"
+name_fr = "ABS-ASA"
+name_es = "ABS-ASA"
 
-bed_temp_degree_c = 60
+bed_temp_degree_c = 100
 
 -- affecting settings to each extruder
 for i = 0, extruder_count-1, 1 do
-  _G['extruder_temp_degree_c_'..i] = 220
+  _G['extruder_temp_degree_c_'..i] = 240
   _G['filament_priming_mm_'..i] = 6.5
   _G['priming_mm_per_sec_'..i] = 40
   _G['retract_mm_per_sec_'..i] = 40
